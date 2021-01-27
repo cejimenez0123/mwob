e46ce9b991b6258dfeebf002dccf0f6abbb092d8
cejimenez0123 <christianjimenez0123@gmail.com>
2021-01-27 14:47:19 -0500

    item

--- mwob/ContentView.swift
+++ mwob/ContentView.swift
@@ -6,7 +6,25 @@
 //
 
 import SwiftUI
-
+struct BlueAndBold:ViewModifier{
+    
+    var text: String
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+                    content
+                        Text(text)
+                        .font(.headline).bold()
+                        .foregroundColor(.white)
+                        .padding(10)
+                        .background(Color.blue)
+                }
+    }
+}
+extension View {
+    func blueAndBold( text: String)->some View{
+        self.modifier(BlueAndBold(text: text))
+    }
+}
 struct GridStack<Content:View>:View{
     let rows: Int
     let columns: Int
@@ -23,23 +41,88 @@ struct GridStack<Content:View>:View{
         }
         
     }
-    
-    
 }
 
-struct ContentView: View {
-    var body: some View {
+
+
+struct ItemView:View{
+    @Environment(\.presentationMode) var presentationMode
+    @State var name = ""
+    @State var price = 0.0
+    var body: some View{
         
-            
-           GridStack(rows: 4, columns: 3) {row,col in
-            VStack{
+        Button("Dismiss"){
+            self.presentationMode.wrappedValue.dismiss()
+        }
+        }
+    
+}
+struct Item: Identifiable,Codable{
+    var id = UUID()
+    let name: String
+    let images:[String]
+    let price: String
+}
+class Basket: ObservableObject{
+    
+    @Published var items=[Item](){
+    didSet{
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(self.items){
                 
-            Text("(\(row),\(col))")
+                UserDefaults.standard.set(encoded,forKey: "items")
+            }
+        }
+            
+        }
+        init(){
+            if let items = UserDefaults.standard.data(forKey: "items"){
+            let decoder = JSONDecoder()
+            if let decoded = try?
+                decoder.decode([Item].self, from: items){
+                self.items = decoded
+            return
+            }
+           
+            }
+    
+        }
+    
+}
+    
+
+struct ContentView: View {
+    @State private var showItem = false
+    var body: some View {
+        NavigationView{
+            VStack{
+                Text("XO")
+                .blueAndBold(text: "Charm")
+                HStack{
+            
+        
+                GridStack(rows: 2, columns: 5) {row,col in
+                    VStack{
+                        Button("Tap Me"){
+                            showItem.toggle()
+                        }.sheet(isPresented: $showItem, content: {
+                            ItemView(name:"Widget",price: 2.0)
+                        })
+                        }
+//                    Text("(\(row),\(col))")
+                        .padding(15)
+                        
             }
            }
         
+        }
+                
+        }
+            
     }
+    
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
